37671cd992dde9d2839e838fbc160a9af12a2962
kushok <74976529+kushok@users.noreply.github.com>
2020-11-24 18:36:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
     print("А это еще один принт")
     print("Третий принт!")
+    print("А вот и четвертый принт")
 
 
 # Press the green button in the gutter to run the script.
